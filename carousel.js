1ad5d27eec056422ee524d5aede00698b08faf29
Shiomi <shiomi.hoshiya@gmail.com>
2023-07-19 17:00:23 +0200

    finished fonction carousel2

--- carousel.js
+++ carousel.js
@@ -77,26 +77,4 @@ const createSwipter = () => {
     },
   });
 };
-// const swiperGrid = new Swiper(".slider2", {
-//   loop: true, //ループ
-//   slidesPerView: 1,
-//   centeredSlides: true,
-//   spaceBetween: 12,
-//   pagination: {
-//     //ページネーション（ドット）
-//     el: ".swiper-pagination",
-//   },
-//   breakpoints: {
-//     //小さい順に設定する
-//     // 599px以上の場合
-//     768: {
-//       slidesPerView: 3,
-//       spaceBetween: 20,
-//     },
-//     // 1024px以上の場合
-//     1024: {
-//       slidesPerView: 3,
-//       spaceBetween: 60,
-//     },
-//   },
-// });
+
